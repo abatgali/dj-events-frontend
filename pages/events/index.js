92626b294457ffc844b5fdf3b8d66a5b514cef96
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-09-09 00:01:14 -0400

    fetching events from json file

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,32 @@
-import Layout from "../../components/Layout"
 
-export default function EventsPage() {
+import EventItem from '@/components/EventItem';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import Link from "next/link";
+
+export default function EventsPage({events}) {
+  // console.log(events);
   return (
-    <Layout title="My Events" >
-        <h1>My Events</h1>
-    </Layout>
+      <Layout>
+          <h1>All Events</h1>
+          {events.length === 0 && <h3>No events to show</h3>}
+
+          {events.map(evt => (
+              <EventItem key={evt.id} evt={evt} />
+          )
+          )}
+
+      </Layout>
+    
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
